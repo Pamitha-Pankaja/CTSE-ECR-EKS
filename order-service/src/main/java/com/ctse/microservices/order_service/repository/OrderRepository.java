816e735a964aa8d86ce73500fc16ca1fa83d5ff7
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2025-05-17 22:26:43 +0530

    Revert "Implemented Order Repository"
    This reverts commit 11dc3b5a5bf3c3fd4ba6728b96a0ad26561135a3.

--- order-service/src/main/java/com/ctse/microservices/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/ctse/microservices/order_service/repository/OrderRepository.java
@@ -4,6 +4,4 @@ package com.ctse.microservices.order_service.repository;
 import com.ctse.microservices.order_service.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface OrderRepository extends MongoRepository<Order, String> {
-    // Inherits methods like save(), findById(), findAll(), deleteById(), etc.
-}
+public interface OrderRepository extends MongoRepository<Order, String> { }
