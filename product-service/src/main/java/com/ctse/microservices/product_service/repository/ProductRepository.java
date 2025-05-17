f6271b4153af24704ae28427e364f14bed17a548
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2025-05-17 22:33:45 +0530

    implemented Product Repository

--- product-service/src/main/java/com/ctse/microservices/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/ctse/microservices/product_service/repository/ProductRepository.java
@@ -3,4 +3,6 @@ package com.ctse.microservices.product_service.repository;
 import com.ctse.microservices.product_service.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {}
+public interface ProductRepository extends MongoRepository<Product, String> {
+    // Inherits methods like save(), findById(), findAll(), deleteById(), etc.
+}
